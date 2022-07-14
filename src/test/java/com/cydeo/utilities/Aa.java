a5a55591e0b1b31b66d6fa1b7c12dac93ab34c21
ahmad <reshad.mohammadi88@yahoo.com>
2022-07-14 16:34:45 -0400

    New Project with Jenkins

--- src/test/java/com/cydeo/utilities/Aa.java
+++
@@ -1,7 +0,0 @@
-package com.cydeo.utilities;
-
-public class Aa {
-    public static void main(String[] args) {
-        System.out.println("Finally");
-    }
-}
--- src/test/java/com/cydeo/utilities/TestClass.java
+++
@@ -1,9 +0,0 @@
-package com.cydeo.utilities;
-
-import com.google.gson.internal.bind.util.ISO8601Utils;
-
-public class TestClass {
-    public static void main(String[] args) {
-        System.out.println("AHmadAHMAD");
-    }
-}
